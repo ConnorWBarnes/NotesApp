d598b4ac63cf5a8bb3d93d6c13f3043e44d72df6
Connor Barnes <Connor.W.Barnes@gmail.com>
2024-06-19 13:56:31 -0700

    Created note-compose-base component and refactored note-create component to extend it

---
+++ frontend/src/app/note/note-compose-base/note-compose-base.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-note-compose-base',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './note-compose-base.component.html',
+  styleUrl: './note-compose-base.component.scss'
+})
+export abstract class NoteComposeBaseComponent {
+  noteTitle = '';
+  noteBody = '';
+  
+  abstract saveButtonText: string;
+
+  abstract save(): void;
+}
--- frontend/src/app/note/note-create/note-create.component.ts
+++ frontend/src/app/note/note-create/note-create.component.ts
@@ -2,20 +2,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { Note } from '../note';
+import { NoteComposeBaseComponent } from '../note-compose-base/note-compose-base.component';
 
 @Component({
   selector: 'app-note-create',
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './note-create.component.html',
-  styleUrl: './note-create.component.scss'
+  templateUrl: './../note-compose-base/note-compose-base.component.html',
+  styleUrl: './../note-compose-base/note-compose-base.component.scss'
 })
-export class NoteCreateComponent {
-  noteTitle = '';
-  noteBody = '';
+export class NoteCreateComponent extends NoteComposeBaseComponent {
+  override saveButtonText: string = 'Create Note';
   @Output() createNoteEvent = new EventEmitter<Note>();
 
-  createNote() {
+  override save(): void {
     // Notify subscriber(s) of the new note to create
     const note: Note = {
       id: '',

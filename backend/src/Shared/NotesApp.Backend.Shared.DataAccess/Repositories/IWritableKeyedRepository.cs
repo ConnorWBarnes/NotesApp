bd380515b24cad3ad616fd00c3a47d3ed6e02231
Connor Barnes <Connor.W.Barnes@gmail.com>
2024-06-27 12:37:41 -0700

    Fixed type parameters for interfaces extened by specification version of IWritableKeyedRepository

--- backend/src/Shared/NotesApp.Backend.Shared.DataAccess/Repositories/IWritableKeyedRepository.cs
+++ backend/src/Shared/NotesApp.Backend.Shared.DataAccess/Repositories/IWritableKeyedRepository.cs
@@ -19,7 +19,7 @@ public interface IWritableKeyedRepository<TEntity, TPrimaryKey> : IReadableKeyed
 /// <typeparam name="TEntity">The type of entity managed by the repository.</typeparam>
 /// <typeparam name="TPrimaryKey">The type of primary key for <typeparamref name="TEntity"/> objects.</typeparam>
 /// <typeparam name="TSpecification">The type of specification to use to query domain entities.</typeparam>
-public interface IWritableKeyedRepository<TEntity, TPrimaryKey, out TSpecification> : IReadableKeyedRepository<TEntity, TPrimaryKey>, IWritableKeyedRepository<TEntity, TPrimaryKey>
+public interface IWritableKeyedRepository<TEntity, TPrimaryKey, out TSpecification> : IReadableKeyedRepository<TEntity, TPrimaryKey, TSpecification>, IWritableRepository<TEntity, TSpecification>
     where TEntity : class, IKeyedEntity<TPrimaryKey>
     where TSpecification : IKeyedSpecification<TSpecification, TEntity, TPrimaryKey>
 {

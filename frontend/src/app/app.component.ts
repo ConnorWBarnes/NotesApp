e5b8f56f19db7e3ffafa7c8af5690b5a5dc564b4
Connor Barnes <Connor.W.Barnes@gmail.com>
2024-06-29 15:42:04 -0700

    Created nav-sidebar component and reorganized app.component.html to position the sidebar correctly

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavSidebarComponent } from './nav-sidebar/nav-sidebar.component';
 import { NoteCreateComponent } from './note/note-create/note-create.component';
 import { NoteEditComponent } from './note/note-edit/note-edit.component';
 import { NoteGridComponent } from './note/note-grid/note-grid.component';
@@ -9,6 +10,7 @@ import { NoteGridComponent } from './note/note-grid/note-grid.component';
   selector: 'app-root',
   standalone: true,
   imports: [
+    NavSidebarComponent,
     NoteCreateComponent,
     NoteEditComponent,
     NoteGridComponent,
---
+++ frontend/src/app/nav-sidebar/nav-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-sidebar.component.html',
+  styleUrl: './nav-sidebar.component.scss'
+})
+export class NavSidebarComponent {
+
+}

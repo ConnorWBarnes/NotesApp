a2307d62e8e02dc7b42a59c3f8d7e2d8f7fa2d85
Connor Barnes <Connor.W.Barnes@gmail.com>
2024-07-29 18:45:16 -0700

    Started updating user-header component to display user info when logged in

--- frontend/src/app/auth/user-header/user-header.component.ts
+++ frontend/src/app/auth/user-header/user-header.component.ts
@@ -1,11 +1,12 @@
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
-import { UserClaim } from '../user-claim';
 import { AuthenticationService } from '../authentication.service';
+import { UserClaim } from '../user-claim';
+import { UserProfile } from '../user-profile';
 
 @Component({
   selector: 'app-user-header',
@@ -21,16 +22,27 @@ import { AuthenticationService } from '../authentication.service';
 })
 export class UserHeaderComponent implements OnInit {
   userClaims$!: Observable<UserClaim[]>;
+  userProfile$!: Observable<UserProfile>;
+  userProfile!: UserProfile;
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
-  ngOnInit(): void {
+  // TODO: Populating user profile on init only works if the component isn't already loaded when navigating from the login component
+  async ngOnInit(): Promise<void> {
     if (this.authService.isLoggedIn) {
       this.userClaims$ = this.authService.getUser$();
+      this.userProfile$ = this.authService.getUserProfile$();
+      this.userProfile = await this.authService.getUserProfileAsync();
+      console.log('User profile: ', this.userProfile);
     }
   }
 
   public get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
+  }
+
+  async logOutAsync() {
+    await this.authService.logOutAsync();
+    this.router.navigateByUrl('/notes');
   }
 }

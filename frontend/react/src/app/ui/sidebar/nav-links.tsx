98af7b27e7c44c7b46f92c9d06aee815e23de6e8
Connor Barnes <Connor.W.Barnes@gmail.com>
2024-09-04 17:31:50 -0700

    Fixed href property for links in nav-links.tsx

--- frontend/react/src/app/ui/sidebar/nav-links.tsx
+++ frontend/react/src/app/ui/sidebar/nav-links.tsx
@@ -1,52 +1,59 @@
 // NavLinks must be a Client Component to use the 'usePathname()' hook
 'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import styles from "@/app/ui/sidebar/sidebar.module.scss";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import styles from '@/app/ui/sidebar/sidebar.module.scss';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
+const notesUrl = '/notes';
 const links = [
   {
-    name: "Notes",
-    href: "/Notes",
-    iconClass: "bi-journal-text",
-    svgFile: "journal-text"
+    name: 'Notes',
+    href: `${notesUrl}`,
+    iconClass: 'bi-journal-text',
+    svgFile: 'journal-text'
   },
   {
-    name: "Create",
-    href: "/Create",
-    iconClass: "bi-journal-plus",
-    svgFile: "journal-plus"
+    name: 'Create',
+    href: `${notesUrl}/create`,
+    iconClass: 'bi-journal-plus',
+    svgFile: 'journal-plus'
   },
   {
-    name: "Archive",
-    href: "/Archive",
-    iconClass: "bi-archive",
-    svgFile: "archive"
+    name: 'Archive',
+    href: `${notesUrl}/archive`,
+    iconClass: 'bi-archive',
+    svgFile: 'archive'
   },
   {
-    name: "Trash",
-    href: "/Trash",
-    iconClass: "bi-trash",
-    svgFile: "trash"
+    name: 'Trash',
+    href: `${notesUrl}/trash`,
+    iconClass: 'bi-trash',
+    svgFile: 'trash'
   }
 ];
 
-function linkIsActive(pathname: string, href: string) {
+/**
+ * Determines whether the given pathname and href are the same string (ignoring case).
+ * @param pathname The current URL's pathname.
+ * @param href The href of the navigation link.
+ * @returns A flag indicating whether the given pathname and href are the same string (ignoring case).
+ */
+function linkIsActive(pathname: string, href: string): boolean {
   return pathname.localeCompare(href, undefined, { sensitivity: 'base' }) === 0;
 }
 
 export default function NavLinks({ isCollapsed }: { isCollapsed: boolean }) {
   const pathname = usePathname();
   return (
-    <ul className="nav nav-pills flex-column mb-auto">
+    <ul className='nav nav-pills flex-column mb-auto'>
       {links.map((link) => {
         return (
-          <li key={link.name} className="nav-item">
+          <li key={link.name} className='nav-item'>
             <Link href={link.href} className={`nav-link m-2 text-white ${styles.sidebar_nav_link} ${linkIsActive(pathname, link.href) ? 'active' : ''}`}>
-              <i className={`bi ${link.iconClass} ${isCollapsed ? '' : 'me-2'}`} style={{fontSize: "1rem"}}></i>
+              <i className={`bi ${link.iconClass} ${isCollapsed ? '' : 'me-2'}`} style={{fontSize: '1rem'}}></i>
               {!isCollapsed ? (link.name) : null}
             </Link>
           </li>
